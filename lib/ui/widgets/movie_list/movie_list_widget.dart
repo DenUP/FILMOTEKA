0743dee05fefbd8581e8ed47655650b75574dc2a
DenUP <ctrike163@gmail.com>
2024-05-26 20:08:08 +0300

    add: Film in Movie List

--- lib/ui/widgets/movie_list/movie_list_widget.dart
+++ lib/ui/widgets/movie_list/movie_list_widget.dart
@@ -1,6 +1,24 @@
 import 'package:filmoteka/Theme/color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
+class Movie {
+  final int id;
+  final String name;
+  final String image;
+  final String rating;
+  final String category;
+  final String data;
+  final String duration;
+
+  Movie(
+      {required this.id,
+      required this.image,
+      required this.name,
+      required this.rating,
+      required this.category,
+      required this.data,
+      required this.duration});
+}
 
 class MovieListWidget extends StatefulWidget {
   const MovieListWidget({super.key});
@@ -10,6 +28,49 @@ class MovieListWidget extends StatefulWidget {
 }
 
 class _MovieListWidgetState extends State<MovieListWidget> {
+  final _movies = [
+    Movie(
+        id: 1,
+        image: 'assets/post/homen3.webp',
+        name: 'Spider',
+        rating: '9.5',
+        category: 'Экшен',
+        data: '2019',
+        duration: '139'),
+    Movie(
+        id: 2,
+        image: 'assets/post/onepluseone.webp',
+        name: '1+1',
+        category: 'Комедия',
+        rating: '8.8',
+        data: '2011',
+        duration: '112'),
+    Movie(
+        id: 3,
+        image: 'assets/post/poimaimeny.webp',
+        name: 'Поймай меня, если сможешь',
+        category: 'Криминал',
+        rating: '8.5',
+        data: '2002',
+        duration: '141'),
+    Movie(
+        id: 4,
+        image: 'assets/post/yiollstreet.webp',
+        name: 'Волк с Уолл-стрит',
+        category: 'Комедия',
+        rating: '8.0',
+        data: '2013',
+        duration: '180'),
+    Movie(
+        id: 5,
+        image: 'assets/post/xatikoo.webp',
+        name: 'Хатико: Самый верный друг',
+        category: 'Драма',
+        rating: '8.4',
+        data: '2008',
+        duration: '89'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,15 +79,16 @@ class _MovieListWidgetState extends State<MovieListWidget> {
       ),
       body: ListView.builder(
         itemExtent: 170,
-        itemCount: 8,
+        itemCount: _movies.length,
         itemBuilder: (context, index) {
+          final movies = _movies[index];
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Row(children: [
               ClipRRect(
                 borderRadius: BorderRadius.circular(18),
                 child: Image.asset(
-                  'assets/post/homen3.webp',
+                  movies.image,
                 ),
               ),
               const SizedBox(
@@ -36,7 +98,7 @@ class _MovieListWidgetState extends State<MovieListWidget> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Spiderman',
+                    movies.name,
                     style: const TextStyle(
                         color: Colors.white,
                         fontSize: 16,
@@ -47,7 +109,7 @@ class _MovieListWidgetState extends State<MovieListWidget> {
                   ),
                   Row(
                     children: [
-                      Icon(
+                      const Icon(
                         Icons.star_border,
                         color: colors.rating,
                       ),
@@ -55,8 +117,8 @@ class _MovieListWidgetState extends State<MovieListWidget> {
                         width: 4,
                       ),
                       Text(
-                        '9.5',
-                        style: TextStyle(
+                        movies.rating,
+                        style: const TextStyle(
                             color: colors.rating,
                             fontSize: 15,
                             fontWeight: FontWeight.w500),
@@ -68,15 +130,15 @@ class _MovieListWidgetState extends State<MovieListWidget> {
                   ),
                   Row(
                     children: [
-                      Icon(
+                      const Icon(
                         Icons.movie_creation_outlined,
                       ),
                       const SizedBox(
                         width: 4,
                       ),
                       Text(
-                        'Action',
-                        style: TextStyle(fontSize: 12),
+                        movies.category,
+                        style: const TextStyle(fontSize: 12),
                       )
                     ],
                   ),
@@ -85,15 +147,15 @@ class _MovieListWidgetState extends State<MovieListWidget> {
                   ),
                   Row(
                     children: [
-                      Icon(
+                      const Icon(
                         Icons.calendar_month_outlined,
                       ),
                       const SizedBox(
                         width: 4,
                       ),
                       Text(
-                        '2019',
-                        style: TextStyle(fontSize: 12),
+                        movies.data,
+                        style: const TextStyle(fontSize: 12),
                       )
                     ],
                   ),
@@ -102,15 +164,15 @@ class _MovieListWidgetState extends State<MovieListWidget> {
                   ),
                   Row(
                     children: [
-                      Icon(
+                      const Icon(
                         Icons.access_time,
                       ),
                       const SizedBox(
                         width: 4,
                       ),
                       Text(
-                        '139 minutes',
-                        style: TextStyle(fontSize: 12),
+                        '${movies.duration} minutes',
+                        style: const TextStyle(fontSize: 12),
                       )
                     ],
                   ),
