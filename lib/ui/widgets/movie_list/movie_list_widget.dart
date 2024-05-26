da58427c7f49bc5a0118765bdee7e78eab42eb68
DenUP <ctrike163@gmail.com>
2024-05-26 19:35:15 +0300

    add: ListView in Movie_list ^_^

--- lib/ui/widgets/movie_list/movie_list_widget.dart
+++ lib/ui/widgets/movie_list/movie_list_widget.dart
@@ -1,5 +1,6 @@
 import 'package:filmoteka/Theme/color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MovieListWidget extends StatefulWidget {
   const MovieListWidget({super.key});
@@ -16,99 +17,107 @@ class _MovieListWidgetState extends State<MovieListWidget> {
         title: const Text('Фильмы'),
       ),
       body: ListView.builder(
-        itemExtent: 150,
+        itemExtent: 170,
         itemCount: 8,
         itemBuilder: (context, index) {
-          return Row(children: [
-            Image.asset('assets/post/homen.png'),
-            const SizedBox(
-              width: 15,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Spiderman',
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700),
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Row(children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(18),
+                child: Image.asset(
+                  'assets/post/homen3.webp',
                 ),
-                const SizedBox(
-                  height: 14,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      color: colors.rating,
-                    ),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    Text(
-                      '9.5',
-                      style: TextStyle(
-                          color: colors.rating,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.local_movies_sharp,
-                    ),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    Text(
-                      'Action',
-                      style: TextStyle(fontSize: 12),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.calendar_month_outlined,
-                    ),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    Text(
-                      '2019',
-                      style: TextStyle(fontSize: 12),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.access_time,
-                    ),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    Text(
-                      '139 minutes',
-                      style: TextStyle(fontSize: 12),
-                    )
-                  ],
-                ),
-              ],
-            )
-          ]);
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Spiderman',
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star_border,
+                        color: colors.rating,
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        '9.5',
+                        style: TextStyle(
+                            color: colors.rating,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.movie_creation_outlined,
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        'Action',
+                        style: TextStyle(fontSize: 12),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.calendar_month_outlined,
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        '2019',
+                        style: TextStyle(fontSize: 12),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.access_time,
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        '139 minutes',
+                        style: TextStyle(fontSize: 12),
+                      )
+                    ],
+                  ),
+                ],
+              )
+            ]),
+          );
         },
       ),
     );
